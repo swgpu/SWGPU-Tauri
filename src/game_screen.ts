e557ef61f04f13406aab1497ff1c17fca123e94a
jay24120 <aliyah.raijin@gmail.com>
2025-07-18 12:40:44 +0200

    same demo of vite quick start

--- src/game_screen.ts
+++ src/game_screen.ts
@@ -1,61 +1,46 @@
 import {
-  gfx3Manager,
-  gfx3DebugRenderer,
-  gfx3MeshRenderer,
-  EnginePack3D,
-  Gfx3CameraWASD,
+  gfx2TextureManager,
+  gfx2Manager,
+  Gfx2SpriteJSS,
+  Tween,
   Screen,
   UT,
+  vec2
 } from 'swgpu';
 // ---------------------------------------------------------------------------------------
 
 class GameScreen extends Screen {
-  camera: Gfx3CameraWASD;
-  pack: EnginePack3D;
+  logo: Gfx2SpriteJSS;
+  animation: Tween<vec2>;
 
   constructor() {
     super();
-    this.camera = new Gfx3CameraWASD(0);
-    this.pack = new EnginePack3D();
+    this.logo = new Gfx2SpriteJSS();
+    this.animation = new Tween<vec2>();
   }
 
   async onEnter() {
-    this.camera.setPosition(0, 0, 10);
-    this.pack = await EnginePack3D.createFromFile('./scene.blend.pak');
+    this.logo.setTexture(await gfx2TextureManager.loadTexture('./textures/logo.png'));
+    this.logo.setOffsetNormalized(0.5, 0.5);
 
-    const cameraPosition = localStorage.getItem('cameraPosition');
-    if (cameraPosition) {
-      const data = JSON.parse(cameraPosition);
-      this.camera.setPosition(data[0], data[1], data[2]);
-    }
-
-    const cameraRotation = localStorage.getItem('cameraRotation');
-    if (cameraRotation) {
-      const data = JSON.parse(cameraRotation);
-      this.camera.setRotation(data[0], data[1], data[2]);
-    }
+    this.animation = new Tween<vec2>([0, 4, 8], [[-250, 0], [250, 0], [-250, 0]], UT.EASE_IN_OUT_QUAD);
+    this.animation.setLooped(true);
   }
 
   update(ts: number) {
-    this.camera.update(ts);
-    this.pack.jsm.forEach(item => item.object.update(ts));
-    localStorage.setItem('cameraPosition', JSON.stringify(this.camera.getPosition()));
-    localStorage.setItem('cameraRotation', JSON.stringify(this.camera.getRotation()));
+    this.logo.update(ts);
+    this.logo.setPosition(this.animation.get(0), this.animation.get(1));
+    this.animation.update(ts);    
   }
 
   draw() {
-    this.pack.jsm.forEach(item => item.object.draw());
-    gfx3MeshRenderer.setAmbientColor([0.5, 0.5, 0.5]);
-    gfx3DebugRenderer.drawGrid(UT.MAT4_ROTATE_X(Math.PI * 0.5), 20, 1);
+    this.logo.draw();
   }
 
-  render(ts: number) {
-    gfx3Manager.beginRender();
-    gfx3Manager.beginPassRender(0);
-    gfx3DebugRenderer.render();
-    gfx3MeshRenderer.render(ts);
-    gfx3Manager.endPassRender();
-    gfx3Manager.endRender();
+  render() {
+    gfx2Manager.beginRender();
+    gfx2Manager.render();
+    gfx2Manager.endRender();
   }
 }
 
